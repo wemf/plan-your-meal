bda6a483ba1eb18280671d1ac4e5c1aa4b94d4ae
William Eduardo Meza <williameduardomeza@gmail.com>
2020-07-27 01:29:20 -0500

    feat: finish add recipe form

---
+++ js/recipes.js
@@ -0,0 +1,17 @@
+fetch('../data/food.json')
+  .then(response => response.json())
+  .then(data => {
+
+      const recipe = document.getElementById('category-recipe');
+
+      data.categories.forEach(category => {
+        let option = document.createElement('option');
+
+        option.setAttribute("value", category.title);
+        option.textContent = category.title;
+    
+        recipe.appendChild(option);
+
+      });
+
+  })
